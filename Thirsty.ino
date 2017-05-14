443944e997605357bc80064c46964e98e2da9f5e
Iulian Arcus <arcusiulian@gmail.com>
2017-05-14 22:17:34 +0100

    Made everything unsigned. Sample in short bursts.

--- Thirsty.ino
+++ Thirsty.ino
@@ -1,74 +1,90 @@
 #include "SoilMoistureSensor.h"
 
-//#define DEBUG
+#define DEBUG
 
-const int iINFORM_LED_PIN = 13;
-const int iTRANSISTOR_PIN = 2;
+using u_int = unsigned int;
+
+// Pins
+const u_int uINFORM_LED_PIN = 13;
+const u_int uTRANSISTOR_PIN = 2;
 
 // Trigger value for notification. Needs more experimentation.
 const float fMAX_DRY_MEASUREMENT = 0.64f;
 
-const int iSENSOR_COUNT = 1;
-SoilMoistureSensor<10> axSMSensors[iSENSOR_COUNT];
+// Sensors
+const u_int uSENSOR_COUNT = 1;
+const size_t uSENSOR_BUFFER = 10;
+SoilMoistureSensor<uSENSOR_BUFFER> axSMSensors[uSENSOR_COUNT];
+
+// Delays
+const u_int uTRANSISTOR_WAKEUP_DELAY = 100;
+const u_int uSAMPLE_DELAY = 50;
+const u_int uLOOP_DELAY = 10000;
  
 void setup() 
 {
-   for (int i=0 ; i<iSENSOR_COUNT; i++)
+  for (int i=0 ; i<uSENSOR_COUNT; i++)
   {
     axSMSensors[i].SetPin(i);
   }
 
    //Power sensor control pin
-   pinMode(iTRANSISTOR_PIN, OUTPUT);
-   digitalWrite(iTRANSISTOR_PIN, LOW); 
+   pinMode(uTRANSISTOR_PIN, OUTPUT);
+   digitalWrite(uTRANSISTOR_PIN, LOW); 
 
    //Notification LED 
-   pinMode(iINFORM_LED_PIN, OUTPUT);
-   digitalWrite(iINFORM_LED_PIN, LOW); 
+   pinMode(uINFORM_LED_PIN, OUTPUT);
+   digitalWrite(uINFORM_LED_PIN, LOW); 
 
    #ifdef DEBUG
    Serial.begin(9600);  
    #endif
 }
  
-void loop() {
-
-  bool bIsReadyToSleep = false;
+void loop()
+{
   // Turn on the sensors
-  digitalWrite(iTRANSISTOR_PIN,HIGH);
+  digitalWrite(uTRANSISTOR_PIN,HIGH);
   // Wait for power to stabilise. (probably not needed?)
-  delay(50);
-  
-  // read the value from the sensors
-  for (int i=0 ; i<iSENSOR_COUNT; i++)
+  delay(uTRANSISTOR_WAKEUP_DELAY);
+
+  for (u_int j=0 ; j<uSENSOR_BUFFER; j++)
   {
-    axSMSensors[i].Sample();
-  
-    if (axSMSensors[i].DoesSensorHaveValue())
+    // read the value from the sensors
+    for (u_int i=0 ; i<uSENSOR_COUNT; i++)
     {
-
-      const float fValue = axSMSensors[i].GetNormalisedValue(); //Between [0, 1)
-
-      #ifdef DEBUG
-      Serial.print("sensor");
-      Serial.print(i);
-      Serial.print(" = ");                       
-      Serial.println(fValue);
-      #endif
-
-      if ( fValue < fMAX_DRY_MEASUREMENT )
-      {
-        digitalWrite(iINFORM_LED_PIN, HIGH);
-      }
-      else
-      {
-        digitalWrite(iINFORM_LED_PIN, LOW); 
-      }
+      axSMSensors[i].Sample();
       
-      axSMSensors[i].ClearStoredValue();
-    }  
-  }   
-    // We're done so turn off the sensors
-    digitalWrite(iTRANSISTOR_PIN,LOW);
-    delay(200);
+      if (axSMSensors[i].DoesSensorHaveValue())
+      {
+        const float fValue = axSMSensors[i].GetNormalisedValue(); //Between [0, 1)
+  
+        #ifdef DEBUG
+        Serial.print("sensor");
+        Serial.print(i);
+        Serial.print(" = ");                       
+        Serial.println(fValue);
+        #endif
+  
+        if ( fValue < fMAX_DRY_MEASUREMENT )
+        {
+          digitalWrite(uINFORM_LED_PIN, HIGH);
+        }
+        else
+        {
+          digitalWrite(uINFORM_LED_PIN, LOW); 
+        }
+        
+        axSMSensors[i].ClearStoredValue();
+      }  
+    }
+    
+    delay(uSAMPLE_DELAY);
+  }
+  
+  // We're done so turn off the sensors
+  digitalWrite(uTRANSISTOR_PIN,LOW);
+
+  // The data shouldn't change that often so leave it alone for a while.
+  delay(uLOOP_DELAY);
 }
